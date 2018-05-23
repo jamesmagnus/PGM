f90642bf0d05a211813259d7075b231a3f12a6cc
Jean-Loup Beaussart <jbeaussart@enseirb-matmeca.fr>
2018-05-23 19:44:48 +0200

    version omp simple

--- fichiers/include/constants.h
+++ fichiers/include/constants.h
@@ -1,25 +1,19 @@
-
 #ifndef CONSTANTS_IS_DEF
 #define CONSTANTS_IS_DEF
 
+#define WIN_WIDTH 992
+#define WIN_HEIGHT 992
 
-//#define WIN_WIDTH   1280
-//#define WIN_HEIGHT  1280
-
-#define WIN_WIDTH   1024
-#define WIN_HEIGHT  1024
-
-#define MAX(a,b) ((a)>(b)?(a):(b))
-#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 #define DEFAULT_DIM 2048
 
 #define DEFAULT_KERNEL "invert"
 
-
 #define RED 0xAA0000FF
 #define YELLOW 0xFFFF00FF
 #define GREEN 0x00AA00FF
-#define BLACK 0
+#define BLACK 0x0
 
 #endif
--- fichiers/src/vie.c
+++ fichiers/src/vie.c
@@ -1,4 +1,3 @@
-
 #include "global.h"
 #include "compute.h"
 #include "graphics.h"
@@ -9,150 +8,183 @@
 
 #include <stdbool.h>
 
+#define COLOR_TILES 1
 
-///////////////////////////// Version séquentielle simple (seq)
-
-static void compute_new_state (int y, int x)
+static void compute_new_state (int y,
+                               int x)
 {
     unsigned n = 0;
 
-    if (x > 0 && x < DIM - 1 && y > 0 && y < DIM - 1) {
-
+    if ((x > 0) && (x < DIM - 1) && (y > 0) && (y < DIM - 1)) {
         for (int i = y - 1; i <= y + 1; i++)
-        for (int j = x - 1; j <= x + 1; j++)
-        n += (cur_img (i, j) == YELLOW);
+            for (int j = x - 1; j <= x + 1; j++)
+                n += (cur_img (i, j) == YELLOW);
 
         if (cur_img (y, x) == YELLOW) {
-            if (n == 3 || n == 4)
-            n = YELLOW;
+            if ((n == 3) || (n == 4))
+                n = YELLOW;
             else
-            n = BLACK;
-        } else {
+                n = BLACK;
+        }
+        else {
             if (n == 3)
-            n = YELLOW;
+                n = YELLOW;
             else
-            n = BLACK;
+                n = BLACK;
         }
 
         next_img (y, x) = n;
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////
 
-// Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
-/*unsigned vie_compute_seq (unsigned nb_iter)
+// Version séquentielle simple
+unsigned vie_compute_seq (unsigned nb_iter)
 {
-for (unsigned it = 1; it <= nb_iter; it ++) {
+    for (unsigned it = 1; it <= nb_iter; it++) {
+        for (int i = 0; i < DIM; i++)
+            for (int j = 0; j < DIM; j++)
+                compute_new_state (i, j);
 
-for (int i = 0; i < DIM; i++) {
-for (int j = 0; j < DIM; j++) {
-compute_new_state (i, j);
-}
+        swap_images ();
+    }
+
+    return 0;
 }
 
-swap_images ();
-}
-
-return 0;
-}*/
+///////////////////////////////////////////////////////////////////////////////
 
 // Version séquentielle avec tuiles
-/*unsigned vie_compute_seq (unsigned nb_iter)
+unsigned vie_compute_seq_tile (unsigned nb_iter)
 {
-for (unsigned it = 1; it <= nb_iter; it ++) {
-for(int tx = 0; tx < DIM/TILEX; ++tx) {
-for(int ty = 0; ty < DIM/TILEY; ++ty) {
-for (int i = tx * TILEX; i < (tx + 1) * TILEX; i++) {
-for (int j = ty * TILEY; j < (ty + 1) * TILEY; j++) {
-compute_new_state (i, j);
-}
-}
-}
-}
+    for (unsigned it = 1; it <= nb_iter; it++) {
+        for (int tx = 0; tx < DIM / TILEX; ++tx)
+            for (int ty = 0; ty < DIM / TILEY; ++ty)
+                for (int i = tx * TILEX; i < (tx + 1) * TILEX; i++)
+                    for (int j = ty * TILEY; j < (ty + 1) * TILEY; j++)
+                        compute_new_state (i, j);
 
-swap_images ();
-}
-
-return 0;
-}*/
-
-bool tile_need_update(int tx, int ty)
-{
-    for(int x = tx - 1; x <= tx + 1; ++x) {
-        for(int y = ty - 1; y <= ty + 1; ++y) {
-            if(x >= 0 && y >= 0 && x < (DIM/TILEX) && y < (DIM/TILEY) && change[x][y])
-            return true;
-        }
+        swap_images ();
     }
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool tile_need_update(int tx,
+                      int ty)
+{
+    for (int x = tx - 1; x <= tx + 1; ++x)
+        for (int y = ty - 1; y <= ty + 1; ++y)
+            if ((x >= 0) && (y >= 0) && (x < (DIM / TILEX)) &&
+                ( y < (DIM / TILEY)) && change[x][y])
+                return true;
     return false;
 }
 
-void color_tile(int tx, int ty, unsigned color) {
-    for (int i = tx * TILEX; i < (tx + 1) * TILEX; i++) {
-        for (int j = ty * TILEY; j < (ty + 1) * TILEY; j++) {
-            if(next_img(i, j) != YELLOW)
-            next_img(i, j) = color;
-        }
-    }
+void color_tile(int tx,
+                int ty,
+                unsigned color)
+{
+    for (int i = tx * TILEX; i < (tx + 1) * TILEX; i++)
+        for (int j = ty * TILEY; j < (ty + 1) * TILEY; j++)
+            if (next_img(i, j) != YELLOW)
+                next_img(i, j) = color;
 }
 
-void swap_changes(){
+void swap_changes()
+{
     bool **tmp = change;
+
     change = next_change;
     next_change = tmp;
 }
 
-// Version séquentielle optimisée avec tuiles
-unsigned vie_compute_seq (unsigned nb_iter)
+void show_tile_color_debug()
 {
-    for (unsigned it = 1; it <= nb_iter; it ++) {
-        for(int tx = 0; tx < DIM/TILEX; ++tx) {
-            for(int ty = 0; ty < DIM/TILEY; ++ty) {
-                next_change[tx][ty] = false;
+    for (int tx = 0; tx < DIM / TILEX; ++tx)
+        for (int ty = 0; ty < DIM / TILEY; ++ty)
+            color_tile(tx, ty, BLACK);
+
+    for (int tx = 0; tx < DIM / TILEX; ++tx) {
+        for (int ty = 0; ty < DIM / TILEY; ++ty)
+            if (next_change[tx][ty]) {
+                color_tile(tx, ty, RED);
+
+                for (int x = tx - 1; x <= tx + 1; ++x)
+                    for (int y = ty - 1; y <= ty + 1; ++y)
+                        if ((x >= 0) && (y >= 0) && (x < (DIM / TILEX)) &&
+                            ( y < (DIM / TILEY)) &&
+                            (( x != tx) || ( y != ty) ) &&
+                            !next_change[x][y])
+                            color_tile(x, y, GREEN);
             }
+    }
+}
+
+void reset_next_change()
+{
+    for (int tx = 0; tx < DIM / TILEX; ++tx)
+        for (int ty = 0; ty < DIM / TILEY; ++ty)
+            next_change[tx][ty] = false;
+}
+
+void update_tile(int tx,
+                 int ty)
+{
+    for (int i = tx * TILEX; i < (tx + 1) * TILEX; i++)
+        for (int j = ty * TILEY; j < (ty + 1) * TILEY; j++) {
+            int prec = cur_img (i, j);
+            compute_new_state (i, j);
+            if (!next_change[tx][ty] &&
+                ((( prec == BLACK) &&
+                  ( next_img(i,
+                             j) == YELLOW) ) ||
+                 (( prec == YELLOW) &&
+                  ( next_img(i, j) == BLACK) )))
+                next_change[tx][ty] = true;
         }
+}
 
-        for(int tx = 0; tx < DIM/TILEX; ++tx) {
-            for(int ty = 0; ty < DIM/TILEY; ++ty) {
-                if(tile_need_update(tx, ty)) {
-                    for (int i = tx * TILEX; i < (tx + 1) * TILEX; i++) {
-                        for (int j = ty * TILEY; j < (ty + 1) * TILEY; j++) {
-                            int prec = cur_img (i, j);
-                            compute_new_state (i, j);
-                            if(!next_change[tx][ty] && ((prec == BLACK && next_img(i, j) == YELLOW) || (prec == YELLOW && next_img(i, j) == BLACK))) {
-                                next_change[tx][ty] = true;
-                            }
-                        }
-                    }
-                }
-                color_tile(tx, ty, BLACK);
-            }
-        }
+// Version séquentielle optimisée avec tuiles
+unsigned vie_compute_seq_tile_opt (unsigned nb_iter)
+{
+    for (unsigned it = 1; it <= nb_iter; it++) {
+        reset_next_change();
+        for (int tx = 0; tx < DIM / TILEX; ++tx)
+            for (int ty = 0; ty < DIM / TILEY; ++ty)
+                if (tile_need_update(tx, ty))
+                    update_tile(tx, ty);
 
-        for(int tx = 0; tx < DIM/TILEX; ++tx) {
-            for(int ty = 0; ty < DIM/TILEY; ++ty) {
-                if(next_change[tx][ty]) {
-                    color_tile(tx, ty, RED);
+#if COLOR_TILES
+        show_tile_color_debug();
+#endif
 
-                    for(int x = tx - 1; x <= tx + 1; ++x) {
-                        for(int y = ty - 1; y <= ty + 1; ++y) {
-                            if(x >= 0 && y >= 0 && x < (DIM/TILEX) && y < (DIM/TILEY) && (x != tx || y != ty) && !next_change[x][y]) {
-                                color_tile(x, y, GREEN);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-
-        swap_changes();
+        swap_changes(); //swap changes and next_changes boolean matrixs
         swap_images();
     }
 
     return 0;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
+// Version OMP simple
+unsigned vie_compute_omp(unsigned nb_iter)
+{
+    for (unsigned it = 1; it <= nb_iter; it++) {
+#pragma omp parallel for collapse(2) schedule(static)
+        for (int i = 0; i < DIM; i++)
+            for (int j = 0; j < DIM; j++)
+                compute_new_state (i, j);
+
+        swap_images ();
+    }
+
+    return 0;
+}
 
 ///////////////////////////// Configuration initiale
 
@@ -163,6 +195,7 @@ void draw_random (void);
 void vie_draw (char *param)
 {
     char func_name [1024];
+
     void (*f)(void) = NULL;
 
     sprintf (func_name, "draw_%s", param);
@@ -178,85 +211,89 @@ void vie_draw (char *param)
 
 static unsigned couleur = 0xFFFF00FF; // Yellow
 
-static void gun (int x, int y, int version)
+static void gun (int x,
+                 int y,
+                 int version)
 {
     bool glider_gun [11][38] =
     {
-        { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0 },
-        { 0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-        { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0 },
+        { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     };
 
     if (version == 0)
-    for (int i=0; i < 11; i++)
-    for(int j=0; j < 38; j++)
-    if (glider_gun [i][j])
-    cur_img (i+x, j+y) = couleur;
+        for (int i = 0; i < 11; i++)
+            for (int j = 0; j < 38; j++)
+                if (glider_gun [i][j])
+                    cur_img (i + x, j + y) = couleur;
 
     if (version == 1)
-    for (int i=0; i < 11; i++)
-    for(int j=0; j < 38; j++)
-    if (glider_gun [i][j])
-    cur_img (x-i, j+y) = couleur;
+        for (int i = 0; i < 11; i++)
+            for (int j = 0; j < 38; j++)
+                if (glider_gun [i][j])
+                    cur_img (x - i, j + y) = couleur;
 
     if (version == 2)
-    for (int i=0; i < 11; i++)
-    for(int j=0; j < 38; j++)
-    if (glider_gun [i][j])
-    cur_img (x-i, y-j) = couleur;
+        for (int i = 0; i < 11; i++)
+            for (int j = 0; j < 38; j++)
+                if (glider_gun [i][j])
+                    cur_img (x - i, y - j) = couleur;
 
     if (version == 3)
-    for (int i=0; i < 11; i++)
-    for(int j=0; j < 38; j++)
-    if (glider_gun [i][j])
-    cur_img (i+x, y-j) = couleur;
-
+        for (int i = 0; i < 11; i++)
+            for (int j = 0; j < 38; j++)
+                if (glider_gun [i][j])
+                    cur_img (i + x, y - j) = couleur;
 }
 
 void draw_stable (void)
 {
-    for (int i=1; i < DIM-2; i+=4)
-    for(int j=1; j < DIM-2; j+=4)
-    cur_img (i, j) = cur_img (i, (j+1)) =cur_img ((i+1), j) =cur_img ((i+1), (j+1)) = couleur;
+    for (int i = 1; i < DIM - 2; i += 4)
+        for (int j = 1; j < DIM - 2; j += 4)
+            cur_img (i, j) = cur_img (i, (j + 1)) = cur_img ((i + 1), j) = cur_img ((i + 1), (j + 1)) = couleur;
 }
 
 void draw_guns (void)
 {
-    memset(&cur_img (0,0), 0, DIM*DIM* sizeof(cur_img (0,0)));
+    memset(&cur_img (0, 0), 0, DIM * DIM * sizeof(cur_img (0, 0)));
 
     gun (0, 0, 0);
-    gun (0,  DIM-1 , 3);
-    gun (DIM - 1 , DIM - 1, 2);
-    gun (DIM - 1 , 0, 1);
+    gun (0, DIM - 1, 3);
+    gun (DIM - 1, DIM - 1, 2);
+    gun (DIM - 1, 0, 1);
 }
 
 void draw_random (void)
 {
-    for (int i=1; i < DIM-1; i++)
-    for(int j=1; j < DIM-1; j++)
-    cur_img (i, j) = (random() & 01) ? couleur : 0;
+    for (int i = 1; i < DIM - 1; i++)
+        for (int j = 1; j < DIM - 1; j++)
+            cur_img (i, j) = (random() & 01) ? couleur : 0;
 }
 
 // Une tête de clown apparaît à l'itération 110
 void draw_clown (void)
 {
-    int i = DIM/2, j = i;
+    int i = DIM / 2, j = i;
 
-    cur_img (i, j-1) = couleur;
+    cur_img (i, j - 1) = couleur;
     cur_img (i, j) = couleur;
-    cur_img (i, j+1) = couleur;
+    cur_img (i, j + 1) = couleur;
 
-    cur_img (i+1, j-1) = couleur;
-    cur_img (i+1, j+1) = couleur;
+    cur_img (i + 1, j - 1) = couleur;
+    cur_img (i + 1, j + 1) = couleur;
 
-    cur_img (i+2, j-1) = couleur;
-    cur_img (i+2, j+1) = couleur;
+    cur_img (i + 2, j - 1) = couleur;
+    cur_img (i + 2, j + 1) = couleur;
 }
