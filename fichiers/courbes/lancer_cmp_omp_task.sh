fd87eda70b972c76eb1662e87eaf55e1bb131334
Jean-Loup Beaussart <jbeaussart@enseirb-matmeca.fr>
2018-06-01 17:35:07 +0200

    courbes !

---
+++ fichiers/courbes/lancer_cmp_omp_task.sh
@@ -0,0 +1,22 @@
+export OMP_NUM_THREADS
+export GOMP_CPU_AFFINITY
+
+ITE=$(seq 10) # nombre de mesures
+THREADS="1 2 4 6 8" # nombre de threads
+SIZES="64 128 256 512 1024 2048 4096" # taille de plateau
+GOMP_CPU_AFFINITY=$(seq 0 8) # vérifier à l'aide de lstopo la bonne alternance des processeurs
+
+PARAM="../prog -n -k vie -i 1000" # parametres commun à toutes les executions
+
+execute (){
+EXE="$PARAM $*"
+OUTPUT="$(echo $* | tr -d ' ')"
+#for nb in $ITE; do for OMP_NUM_THREADS in $THREADS ; do echo -n "$OUTPUT $OMP_NUM_THREADS " >> ALL ;  $EXE | tail -n1 >> ALL; done; done
+for SIZE in $SIZES; do for nb in $ITE ; do echo -n "$OUTPUT $SIZE " >> ALL_omp_task ;  $EXE -s $SIZE | tail -n1 >> ALL_omp_task; done; done
+}
+
+#for i in 256 512 1024 2048;
+#do
+execute  -v omp_tile_task
+execute  -v omp_tile_task_opt
+#done
